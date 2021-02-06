16ce17495f38e2763d2c1cd31047eaea8e5ba193
Luiz Felipe <luizfelipebasile@gmail.com>
2021-02-06 16:45:13 -0300

    Updates

---
+++ precache-manifest.130449bc91208a344ed601016dd27e96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a0e27b189814f14188c",
+    "url": "/me/static/css/main.2b816397.chunk.css"
+  },
+  {
+    "revision": "9a0e27b189814f14188c",
+    "url": "/me/static/js/main.9a0e27b1.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "b9e8cbf3bf82bb3a7334f09576de2d3b",
+    "url": "/me/index.html"
+  }
+];
